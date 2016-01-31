b761680cb9cbc952c6a1a8a17bf0817d71184b54
Peter Rudolfsen <peter@silencethefoe.com>
2016-01-31 20:15:51 +0100

    Fix retries

--- post.js
+++ post.js
@@ -1,16 +1,30 @@
 'use strict';
 
 const request = require('request-prom');
+const program = require('commander');
+const pack = require('./package.json');
+
+program
+  .version(pack.version)
+  .option('-w, --wait-time <n>', 'Seconds to wait between requests', 5)
+  .option('-i, --index [index]', 'Index to start at', 0)
+  .parse(process.argv);
 
 const url = 'https://bookstore-41.myshopify.com/admin/smart_collections.json';
-const waitTimeSeconds = 5;
 
 let collections = require('./collections.json');
+const totalCollections = collections.length;
 
-console.log(`${collections.length} to send. Sending 1 request per ${waitTimeSeconds} seconds`);
+program.index = parseInt(program.index, 10);
+if (program.index > 0) {
+    collections = collections.slice(program.index);
+}
 
+console.log(`${collections.length} to send. Sending 1 request per ${program.waitTime} seconds`);
+
+let currentIndex = program.index;
 function post() {
-    const collection = collections.pop();
+    const collection = collections.shift();
 
     const opts = {
         url: url,
@@ -23,20 +37,20 @@ function post() {
         }
     };
 
-    console.log('Sending collection...');
-
     return request(opts);
 }
 
 function doPost() {
     post()
         .then(() => {
-            console.log('Sent!', collections.length, 'left.');
-            setTimeout(doPost, waitTimeSeconds * 1000);
+            console.log(`${currentIndex}/${totalCollections}`);
+            currentIndex += 1;
+            setTimeout(doPost, program.waitTime * 1000);
         })
         .catch((error) => {
-            console.log('Request failed', error.response);
-        })
+            console.log(`Request failed at ${currentIndex}`);
+            console.log(`Run node post.js -i ${currentIndex} to continue/try again`);
+        });
 }
 
 doPost();
